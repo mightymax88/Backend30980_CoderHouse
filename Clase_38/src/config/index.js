6c7d31cf3595fb021ffd1da774513a3688caa687
Maximiliano Filipuzzi <maxi.filipuzzi@gmail.com>
2022-09-11 18:37:27 -0300

    Desafio clase 38

---
+++ Clase_38/src/config/index.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv').config();
+
+module.exports = {
+    // FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+    FACEBOOK_CLIENT_ID: 'asd1das121rw',
+    // FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+    FACEBOOK_CLIENT_SECRET: 'asd12dasd',
+    PORT: process.env.PORT,
+    CEL_PHONE_NUMBER: process.env.CEL_PHONE_NUMBER
+}
